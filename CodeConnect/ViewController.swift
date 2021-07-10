06c197f7d028ad341247a6250c8849d6b82b3f42
ArunJack <arun.tell6@gmail.com>
2021-07-10 23:32:48 +0530

    Utilised  custom component - AJTextField.

--- CodeConnect/ViewController.swift
+++ CodeConnect/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
-
+import AJCoreComponent
+import SnapKit
 class ViewController: UIViewController {
+
+    let customField = AJTextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        build()
+    }
+    
+    func build() {
+        self.view.addSubview(customField)
+        
+        customField.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.leftMargin.rightMargin.equalToSuperview()
+            make.height.equalTo(70)
+        }
+        customField.borderColor = .brown
+        customField.cornerRadius = 3.0
+        customField.placeholder = "Username"
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CodeConnect' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+    pod 'SnapKit'
+    pod 'AJCoreComponent', :git => 'git@github.com:Arunjack/AJCoreComponent.git', :branch => 'master'
+    
+  # Pods for CodeConnect
+
+  target 'CodeConnectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CodeConnectUITests' do
+    # Pods for testing
+  end
+
+end
